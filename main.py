d66548cff5f5c0750e3ed98d4d3cd84ae071778d
madiha8902 <madiha.batool@bellfield.nsw.edu.au>
2024-10-14 20:12:51 +1100

    Data structure in pyton 1

---
+++ main.py
@@ -0,0 +1,32 @@
+# name="madiha"
+# no =10
+# lest=["apple","orange","banana","peach"]
+# print(len(lest))
+# print("1st element",lest[0])
+# print("last element",lest[3])
+# lest.append("peach")
+# print(lest)
+
+# lest.remove("banana")
+# print(lest)
+
+# lest.sort()
+# print("sort==>",lest)
+
+# lest.pop(1)
+# print("aftr popping==>",lest)
+
+# lest.reverse()
+# print("reversed list==>",lest)
+
+# lest.clear()
+# print(lest)
+
+mydict={"name":"madiha","age":14,"grade":9}
+print(mydict["name"])
+print(mydict["age"])
+print(mydict["grade"])
+mydict["address"]="sydney"
+print(mydict)
+mydict.pop("age")
+print(mydict)
